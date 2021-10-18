e111fa57c912f05a5622ba016c68b86e5efcb111
FarruxTGP <farruhlee02@gmail.com>
2021-10-18 13:48:10 +0500

    quiz

--- assets/js/swip.js
+++ assets/js/swip.js
@@ -19,4 +19,64 @@ $(".tab-link").click(function () {
     .siblings()
     .removeClass("activec");
 });
+$("#cost").click(function(){
+  $("#sps__calc").show(300)
+})
+$(".closer").click(function(){
+  $("#sps__calc").hide(300)
+})
+$("#sps__calc").hide()
+
+
+
+// $("#quiz1").hide()
+$("#quiz2").hide()
+$("#quiz3").hide()
+$("#quiz4").hide()
+$("#quiz5").hide()
+$(".quiz6").hide()
+
+// $("#submit1").hide()
+$("#submit2").hide()
+$("#submit3").hide()
+$("#submit4").hide()
+$("#submit5").hide()
+
+
+$("#submit1").click(function(){
+  $("#quiz1").hide()
+  $("#submit1").hide()
+  $("#quiz2").show()
+  $("#submit2").show()
+})
+
+
+$("#submit2").click(function(){
+  $("#quiz2").hide()
+  $("#submit2").hide()
+  $("#quiz3").show()
+  $("#submit3").show()
+})
+
+$("#submit3").click(function(){
+  $("#quiz3").hide()
+  $("#submit3").hide()
+  $("#quiz4").show()
+  $("#submit4").show()
+})
+
+
+$("#submit4").click(function(){
+  $("#quiz4").hide()
+  $("#submit4").hide()
+  $("#quiz5").show()
+  $("#submit5").show()
+})
+
+
+$("#submit5").click(function(){
+  $("#quiz5").hide()
+  $("#submit1").hide()
+  $(".quiz6").show()
+})
 document.querySelectorAll(".tab-link")[0].classList.add("act");
